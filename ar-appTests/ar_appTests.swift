ae3f060f00cb08d188b88afeabf4c6967659ac44
junow <junow@rainist.com>
2020-12-15 23:49:45 +0900

    Initial Commit

---
+++ ar-appTests/ar_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  ar_appTests.swift
+//  ar-appTests
+//
+//  Created by junow on 2020/12/15.
+//
+
+import XCTest
+@testable import ar_app
+
+class ar_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
